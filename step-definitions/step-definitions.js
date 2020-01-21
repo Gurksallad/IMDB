cc9c5c9dcf92390bd1f1427e0b4db3b9c014e1f4
marian <marian99@live.se>
2020-01-21 16:22:57 +0100

    färdig version 2 scenarios
    TobyMax+Ida&Marian

--- step-definitions/step-definitions.js
+++ step-definitions/step-definitions.js
@@ -4,14 +4,23 @@ module.exports = function () {
 
   //Feature Sökfunktion/Kategorier background
   let searchField
-  let sleepTime = 3000;
-  this.Given(/^That I’m on the platform IMDb.com$/, async function () {
+  let sleepTime = 2000;
+  let regEmail
+
+  /*this.Given(/^That I’m on the platform IMDb.com$/, async function () {
     await helpers.loadPage('https://imdb.com');
     await sleep(sleepTime)
     searchField = await $('#suggestion-search')
     assert.instanceOf(searchField, searchField.constructor, "Expected a web element")
   });
+*/
+  this.Given(/^That I’m on the platform IMDb\.com$/, async function () {
+    await helpers.loadPage('https://imdb.com');
+    await sleep(1000)
+    searchField = await $('#home_img_holder')
+    assert.instanceOf(searchField, searchField.constructor, "Expected a web element")
 
+  });
   this.When(/^I enter the title "([^"]*)"$/, async function (searchText) {
     searchField = await $('#suggestion-search');
     assert(searchField, 'Can not find the search field on the page');
@@ -44,4 +53,81 @@ module.exports = function () {
     assert.include(resultText, title, 'Could not find the phrase ' + title + ' in the first search result.');
     await sleep(sleepTime);
   });
+
+
+  
+
+  this.When(/^i press the button sign in$/, async function () {
+    let signIn = await $('.imdb-header__signin-text')
+    signIn.click()
+    await sleep(sleepTime)
+  });
+
+  this.When(/^press the button Create a New Account$/, async function () {
+    let createButton = await $('#signin-options > div > div:nth-child(4) > a')
+    createButton.click()
+    await sleep(sleepTime)
+  });
+
+  this.When(/^I enter my name "([^"]*)"$/, async function (name) {
+    let nameBox = await $('#ap_customer_name')
+    await nameBox.sendKeys(name)
+    await sleep(sleepTime)
+  });
+
+
+
+
+  this.When(/^I enter a email adress$/, async function () {
+    let emailInput = await $('#ap_email')
+    // We can use create multiple "emails" that go to the same gmail account
+    // using the plus sign see:
+    // http://www.codestore.net/store.nsf/unid/BLOG-20111201-0411
+    regEmail = 'bertil.bertilsdotter+' + Date.now() + '@gmail.com';
+    console.log("USING THE FOLLOWING EMAIL FOR REGISTRATION ", regEmail)
+    await emailInput.sendKeys(regEmail)
+    await sleep(sleepTime)
+  });
+
+
+
+  this.When(/^I type in a password "([^"]*)"$/, async function (password) {
+    let passwordInput = await $('#ap_password')
+    await passwordInput.sendKeys(password)
+    await sleep(sleepTime)
+  });
+
+
+
+
+  this.When(/^I re\-enter the same password "([^"]*)"$/, async function (reEnterPassword) {
+    let reEnterPasswordInput = await $('#ap_password_check')
+    await reEnterPasswordInput.sendKeys(reEnterPassword)
+    await sleep(sleepTime)
+  });
+
+
+
+  this.When(/^I press Create your IMDb account$/, async function () {
+    let continueButton = await $('#continue')
+    continueButton.click()
+    await sleep(sleepTime)
+  });
+
+
+  this.Then(/^I should be automatically logged in to my account with the name "([^"]*)"$/, async function (name) {
+    // Grab the label element where the user name is shown
+    let loggedInLabel = await $('label[for="navUserMenu"] .ipc-button__text')
+    // Get the text inisde the label
+    let textInLabel = await loggedInLabel.getText();
+    // Compare the text from the label with the name we registrered as
+    assert.strictEqual(textInLabel, name, 'Not logged in with the name "' + name + '"');
+  });
+
+
 }
+
+
+
+
+
