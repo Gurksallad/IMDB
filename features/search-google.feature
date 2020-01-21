56a82fdee0ae6e3b6955876a2e7de155734a678e
Max <Maxfalk_95@hotmail.com>
2020-01-21 11:42:28 +0100

    Scenario för titelsökning
    Updated

--- features/search-google.feature
+++ features/search-google.feature
@@ -1,19 +1,12 @@
-Feature:
+Feature: searchfunction/categories
   As a user I want to search for titles, TV-episodes, celebs, companies and keywords
   to get a relevant result using the english language
 
-  Background:
-
-    Given  That I’m on the platform IMDb.com
-
-
-  Scenario:
-    When jag söker på något, ska resultat som innehåller sökordet visas
-
+Background:
+  Given  That I’m on the platform IMDb.com
 
 Scenario: As a user i want to be able to search on titles on IMDB
-  When I enter a title
-  And I press the categories button
-  And I press Titles
+  When I enter the title "Lion King"
+  And I press the categories button titles
   When I press the search button
-  Then I should see the search results based on titles
+  Then I should see the search results based on the title
--- step-definitions/search-google.js
+++ step-definitions/search-google.js
@@ -1,51 +1,35 @@
 let { $, sleep } = require('./funcs');
 
 module.exports = function () {
-  /*
-    let searchField
-  
-    this.Given(/^that I'm at the search page$/, async function () {
-      await helpers.loadPage('https://google.com');
-      await sleep(1000)
-      searchField = await $('input.gLFyf')
-      assert.instanceOf(searchField, searchField.constructor, "Expected a web element")
-    });
-  
-    this.When(/^I enter search text "([^"]*)"$/, async function (searchString) {
-      await searchField.sendKeys(searchString)
-      await sleep(3000)
-    });
-  
-    this.When(/^I click the search button$/, async function () {
-      let button = await $('div.tfB0Bf:nth-child(7) > center:nth-child(2) > input:nth-child(1)')
-      button.click()
-      await sleep(4000)
-    });
-  
-    this.Then(/^the first test result should contain the word "([^"]*)"$/, async function (searchString) {
-      let results = await $('h3')
-      let found = false
-      for(let result of results){
-        console.log('text', await result.getText())
-        let text = await result.getText()
-        if( text.toLowerCase().includes( searchString.toLowerCase() ) ){
-          found = true
-          break
-        }
-      }
-  
-      assert(found, 'expected to find ' + searchString)
-  
-    });
-  */
-  let searchField
 
-  this.Given(/^That I’m on the platform IMDb\.com$/, async function () {
+//Feature Sökfunktion/Kategorier background
+  let searchField
+  this.Given(/^That I’m on the platform IMDb.com$/, async function () {
     await helpers.loadPage('https://imdb.com');
     await sleep(1000)
-    searchField = await $('#home_img_holder')
+    searchField = await $('#suggestion-search')
     assert.instanceOf(searchField, searchField.constructor, "Expected a web element")
+  });
+
+//Scenario: As a user i want to be able to search on titles on IMDB start
+  this.When(/^I enter a title$/, function () {
 
   });
+
+  this.When(/^I press the categories button$/, function () {
+
+  });
+  this.When(/^I press Titles$/, function () {
+
+  });
+
+  this.When(/^I press the search button$/, function () {
+
+  });
+
+  this.Then(/^I should see the search results based on titles$/, function () {
+
+  });
+//Scenario: As a user i want to be able to search on titles on IMDB end
 }
 
